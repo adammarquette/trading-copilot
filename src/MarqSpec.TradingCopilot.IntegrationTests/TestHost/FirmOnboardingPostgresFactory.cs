ed3545fb5a3754d2b93f87fda427aad4500da397
Marquette Specifications <adam.marquette@gmail.com>
2026-07-22 20:28:57 -0500

    test(qa): risk profile declaration & retrieval integration test suite (#10)
    Assisted-by: Antigravity

--- src/MarqSpec.TradingCopilot.IntegrationTests/TestHost/FirmOnboardingPostgresFactory.cs
+++ src/MarqSpec.TradingCopilot.IntegrationTests/TestHost/FirmOnboardingPostgresFactory.cs
@@ -1,42 +1,8 @@
-using MarqSpec.TradingCopilot.Api.Venues;
-using MarqSpec.TradingCopilot.Domain.Venue;
-using MarqSpec.TradingCopilot.IntegrationTests.Api;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.DependencyInjection;
-
 namespace MarqSpec.TradingCopilot.IntegrationTests.TestHost;
 
 /// <summary>
-/// Subclass of <see cref="PostgresApiFactory"/> equipping integration tests with the adversarial venue stub
-/// and configurable deployment environment settings.
+/// Subclass of <see cref="StubbedVenuePostgresFactory"/> kept for firm onboarding test compatibility.
 /// </summary>
-public class FirmOnboardingPostgresFactory : PostgresApiFactory
+public class FirmOnboardingPostgresFactory : StubbedVenuePostgresFactory
 {
-    public WebApplicationFactory<Program> CreateHost(DeploymentEnvironment environment)
-    {
-        return WithWebHostBuilder(builder =>
-        {
-            builder.UseEnvironment(environment.ToString());
-        });
-    }
-
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
-    {
-        base.ConfigureWebHost(builder);
-        builder.UseSetting("ProjectX:CredentialKey", "topstep-main");
-    }
-
-    protected override void ConfigureTestServices(IServiceCollection services)
-    {
-        base.ConfigureTestServices(services);
-
-        ServiceDescriptor? factoryDescriptor = services.FirstOrDefault(d => d.ServiceType == typeof(IProjectXVenueFactory));
-        if (factoryDescriptor is not null)
-        {
-            services.Remove(factoryDescriptor);
-        }
-
-        services.AddScoped<IProjectXVenueFactory, AdversarialTestProjectXVenueFactory>();
-    }
 }
